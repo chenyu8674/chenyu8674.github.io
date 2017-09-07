3ef3755593208d000a91b5b0912fc170e6e53c4d
chenyu <chenyu@rbxd.com>
2017-09-07 11:32:43 +0800

    测试

--- js-games/brain_wave_demo.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var brainMode = decodeURI(GetQueryString("brain"));
-    if (brainMode == "thinkgear") {
-        Config.titleText = ["脑电测试", "", ""];
-    } else if (brainMode == "hht") {
-        Config.titleText = ["脑血流测试", "", ""];
-    }
-});
