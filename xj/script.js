5518f7dad231ad6fa02075b66b4ae470482fe74c
chenyu <chenyu@rbxd.com>
2017-12-18 20:30:52 +0800

    Signed-off-by: chenyu <chenyu@rbxd.com>

--- xj/script.js
+++ xj/script.js
@@ -40,6 +40,7 @@ var searchResultList = [];// 搜索结果数据
 var editMode = 0;// 数据编辑模式，1添加 2修改
 var editIndex = -1;// 当前修改的数据索引
 var showIndex = -1;// 当前显示内容标记，0原始数据 -1搜索结果 
+var tableMode = 1;
 
 // 打开编辑窗口（新增）
 function show_add_window() {
@@ -123,7 +124,8 @@ function do_clear_data() {
 
 // 重绘原始数据表格
 function create_raw_table() {
-    // $("#btn_base_data").html("基础信息");
+    tableMode = 1;
+    $("#btn_base_data").html("基础信息");
     setvalue("searchinput0", "");
     setvalue("searchinput1", "");
     setvalue("searchinput2", "");
@@ -134,6 +136,7 @@ function create_raw_table() {
     $("#search_type_view").hide();
     $("#search_month_view").hide();
     $("#data_table").html("");
+    create_output_raw_table();
     if (rawDataList.length == 0) {
         return;
     }
@@ -158,6 +161,29 @@ function create_raw_table() {
             + "<td>" + info + "</td>"
             + "<td><button class='button1' onclick='edit_raw_data(" + i + ")'>改</button><button class='button1' onclick='delete_raw_data(" + i + ")'>删</button></td>"
         + "</tr>").appendTo($("#data_table"));
+    }
+}
+
+function create_output_raw_table() {
+    $("#raw_table").html("");
+    $("<tr><td>患者姓名</td><td>手机号码</td><td>科室名称</td><td>专家姓名</td><td>手术名称</td><td>手术时间</td><td>手术费用</td><td>所属医助</td><td>备注</td></tr>").appendTo($("#raw_table"));
+    for(var i in rawDataList) {
+        var data = rawDataList[i];
+        var info = data[8];
+        if (info == null || info == "") {
+            info = "";
+        }
+        $("<tr>"
+            + "<td>" + data[0] + "</td>"
+            + "<td>" + data[1] + "</td>"
+            + "<td>" + data[2] + "</td>"
+            + "<td>" + data[3] + "</td>"
+            + "<td>" + data[4] + "</td>"
+            + "<td>" + data[5] + "</td>"
+            + "<td>" + data[6] + "</td>"
+            + "<td>" + data[7] + "</td>"
+            + "<td>" + info + "</td>"
+        + "</tr>").appendTo($("#raw_table"));
     }
 }
 
@@ -260,7 +286,8 @@ function search_month_next() {
 
 // 重绘搜索结果表格
 function create_search_table() {
-    // $("#btn_base_data").html("搜索结果");
+    tableMode = 2;
+    $("#btn_base_data").html("搜索结果");
     $("#search_type_view").show();
     $("#search_month_view").show();
     $("#data_table").html("");
@@ -285,6 +312,7 @@ function create_search_table() {
 
 // 重绘总体统计表格
 function create_statistics_table() {
+    tableMode = 3;
     $("#data_table").html("");
     $("<tr><td>总体费用合计</td><td>总体绩效合计</td><td>其他人员绩效</td></tr>").appendTo($("#data_table"));
     var data0 = 0;
@@ -305,6 +333,7 @@ function create_statistics_table() {
 
 // 重绘医助统计表格
 function create_assistant_table() {
+    tableMode = 4;
     $("#data_table").html("");
     $("<tr><td>医助名称</td><td>费用合计</td><td>绩效合计</td></tr>").appendTo($("#data_table"));
     var assistantList = [];
@@ -331,6 +360,7 @@ function create_assistant_table() {
 
 // 重绘专家统计表格
 function create_expert_table() {
+    tableMode = 5;
     $("#data_table").html("");
     $("<tr><td>专家名称</td><td>费用合计</td></tr>").appendTo($("#data_table"));
     var expertList = [];
@@ -356,6 +386,7 @@ function create_expert_table() {
 
 // 重绘科室统计表格
 function create_department_table() {
+    tableMode = 6;
     $("#data_table").html("");
     $("<tr><td>科室名称</td><td>费用合计</td></tr>").appendTo($("#data_table"));
     var departmentList = [];
@@ -377,4 +408,142 @@ function create_department_table() {
         + "<td>" + departmentList[department].toFixed(2) + "</td>"
     + "</tr>").appendTo($("#data_table"));
     }
+}
+
+// 导出数据
+function export_raw_table() {
+    var month = $("#search_month_text").text();
+    if (tableMode == 1) {
+        if (rawDataList.length == 0) {
+            return;
+        }
+        tabletoexcel("raw_table", "原始数据");
+    } else if (tableMode == 2) {
+        tabletoexcel("data_table", month + "-搜索结果");
+    } else if (tableMode == 3) {
+        tabletoexcel("data_table", month + "-总体统计");
+    } else if (tableMode == 4) {
+        tabletoexcel("data_table", month + "-医助统计");
+    } else if (tableMode == 5) {
+        tabletoexcel("data_table", month + "-专家统计");
+    } else if (tableMode == 6) {
+        tabletoexcel("data_table", month + "-科室统计");
+    }
+}
+
+function tabletoexcel(tableid, tablename) {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    if (userAgent.indexOf("Chrome") > -1){
+        var table = document.getElementById(tableid);
+        var uri = 'data:application/vnd.ms-excel;base64,';
+        var template = '<html><head><meta charset="UTF-8"></head><body><table>'+table.innerHTML+'</table></body></html>';
+        var encode = encodeURIComponent(template);
+        var unesca = unescape(encode);
+        uri = uri + window.btoa(unesca);
+        var exportExcel = document.getElementById("export");
+        exportExcel.href = uri;
+        var time = new Date().getTime();
+        time = timetostring(time);
+        exportExcel.download = tablename + "_" + time;
+        exportExcel.click();
+    } else {
+        alert("当前浏览器不支持数据导出，请使用Chrome浏览器");
+    }
+}
+
+// 时间戳转显示用时间文字
+function timetostring(time) {
+    var date = new Date(time);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1 + "";
+    if (month.length < 2) {
+        month = "0" + month;
+    }
+    var day = date.getDate() + "";
+    if (day.length < 2) {
+        day = "0" + day;
+    }
+    var hour = date.getHours() + "";
+    if (hour.length < 2) {
+        hour = "0" + hour;
+    }
+    var minute = date.getMinutes() + "";
+    if (minute.length < 2) {
+        minute = "0" + minute;
+    }
+    var second = date.getSeconds() + "";
+    if (second.length < 2) {
+        second = "0" + second;
+    }
+    return year + month + day + hour + minute + second;
+}
+
+function import_raw_table() {
+    var inputObj = $('#import_file');
+    var fileList = inputObj[0].files;
+    if (typeof(FileReader) !== "undefined") {
+        var reader = new FileReader();
+        try {
+            var fileName = fileList[0].name;
+            reader.readAsText(fileList[0]);
+            reader.onload = function(evt) {
+                var data = evt.target.result;
+                var dataList = getstrarray(data, "<tr>", "</tr>", true, false);
+                if (dataList.length == 0) {
+                    alert("仅支持原始数据导入");
+                    return;
+                }
+                rawDataList = [];
+                for (var i = 1; i < dataList.length; i++) {
+                    var tdList = getstrarray(dataList[i], "<td>", "</td>", true, false);
+                    if (tdList.length != 9) {
+                        alert("仅支持原始数据导入");
+                        return;
+                    }
+                    if (i == 1) {
+                        rawDataList = [];
+                    }
+                    var data = [];
+                    for (var j = 0; j < tdList.length; j++) {
+                        data.push(tdList[j].replace("<td>", ""));
+                    }
+                    rawDataList.push(data);
+                }
+                create_raw_table();
+            }
+            $('#import_file').val("");
+        } catch(e) {}
+    }
+}
+
+function getstr(v,s,e,a,b){
+    var r="";
+    var t=v.indexOf(s);
+    if(t<0){
+        return "";
+    }
+    if(a){
+        r=v.substr(t);
+    }else{
+        r=v.substr(t+s.length);
+    }
+    t=r.indexOf(e);
+    if(b){
+        r=r.substr(0,t+e.length);
+    }else{
+        r=r.substr(0,t);
+    }
+    return r;
+}
+
+function getstrarray(v,s,e,a,b){
+    var r=[];
+    var t=getstr(v,s,e,a,b);
+    while(t!=""){
+        r.push(t);
+        t=getstr(v,s,e,1,1);
+        v=v.substr(v.indexOf(t)+t.length);
+        t=getstr(v,s,e,a,b);
+    }
+    return r;
 }
