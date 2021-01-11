c6ef543b10875be678616731e7d476a4b9c56101
yu.chen <13681316820@163.com>
2021-01-11 11:33:58 +0800

    Signed-off-by: yu.chen <13681316820@163.com>

--- wow-offline/js/script/setting.js
+++ wow-offline/js/script/setting.js
@@ -1,14 +1,17 @@
+// 基础速度系数
+let ONLINE_MULTIPLE = 10;
+
 // 调试加速系数
 let LOCAL_MULTIPLE = 1;
 
 // 回合时间
-let TURN_TIME = 1000;
+let TURN_TIME = 1000 / ONLINE_MULTIPLE;
 
 // 食用补给0~100回复时间
-let HEAL_TIME = 10;
+let HEAL_TIME = 10 / ONLINE_MULTIPLE;
 
 // 移动距离
-let MOVE_DISTANCE = 5;
+let MOVE_DISTANCE = 5 * ONLINE_MULTIPLE;
 
 // 经验系数
 let EXP_MULTIPLE = is_in_local_mode() ? 0 : 1;
